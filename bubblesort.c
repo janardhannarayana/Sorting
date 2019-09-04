266777a7a18e45215cd7cd942e8b0a1c83de0e8c
janardhannarayana <janardhannarayana@gmail.com>
2019-09-04 09:25:29 +0530

    Bubble sort:
    Added recursive method.

--- bubblesort.c
+++ bubblesort.c
@@ -3,6 +3,26 @@
 
 #define MAX_ARRAY 1000
 
+void bubble_sort_recursive(int *a, int n);
+void swap(int *x, int *y);
+void bubble_sort(int *a);
+
+
+void bubble_sort_recursive(int *a, int n)
+{
+    int i = 0;
+
+    if (n == 0)
+	return;
+
+    for (i = 0; i < n - 1; i++) {
+	if(a[i] > a[i+1]) {
+	    swap(&a[i], &a[i+1]);
+	}
+    }
+    bubble_sort_recursive(a, n - 1);
+}
+
 void swap(int *x, int *y)
 {
     *x = *x + *y;
@@ -33,10 +53,11 @@ int main()
 	printf("%d\t",a[i]);
     }
     printf("\nSORTED ARRAY\n");
-    bubble_sort(a);
+//    bubble_sort(a);
+    bubble_sort_recursive(a, MAX_ARRAY);
     for(i = 0; i < MAX_ARRAY; i++) {
 	printf("%d\t",a[i]);
     }
     printf("\n");
-	return 0;
+    return 0;
 }
