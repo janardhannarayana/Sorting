2be0e3d82e96f4aee50de49860218f1adabe3c1d
janardhannarayana <janardhannarayana@gmail.com>
2019-09-11 09:50:02 +0530

    merge sort correction

--- merge_sort.c
+++ merge_sort.c
@@ -1,56 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_ARRAY 5
+#define MAX_ARRAY 1000
 
 void merge_sort(int *a, int start, int end);
 void merge(int *a, int start, int mid, int end);
 
 void merge(int *a, int start, int mid, int end)
 {
-  int temp[end - start + 1];
+    int temp[end - start + 1];
 
-  int i = start;
-  int j = mid + 1;
-  int k =0;
+    int i = start;
+    int j = mid + 1;
+    int k =0;
 
-  while( i <= mid && j <= end) {
-    if (a[i] <= a[j]) {
-      temp[k] = a[i];
-      i++;
-      k++;
-    } else if (a[i] > a[j]) {
-      temp[k] = a[j];
-      j++;
-      k++;      
+    while( i <= mid && j <= end) {
+	if (a[i] <= a[j]) {
+	    temp[k] = a[i];
+	    i++;
+	    k++;
+	} else if (a[i] > a[j]) {
+	    temp[k] = a[j];
+	    j++;
+	    k++;
+	}
     }
-  }
 
-  while(i <= mid) {
-    temp[k] = a[i];
-    i++;
-    k++;
-  }
+    while(i <= mid) {
+	temp[k] = a[i];
+	i++;
+	k++;
+    }
 
-  while(j <= end) {
-    temp[k] = a[j];
-    j++;
-    k++;
-  }
-  for(i = start; i <= end;i++) {
-    a[i] = temp[i];
-  }
-  return;
+    while(j <= end) {
+	temp[k] = a[j];
+	j++;
+	k++;
+    }
+    
+    for(i = start, k = 0; i <= end && k <= (end - start + 1);i++, k++) {
+	a[i] = temp[k];
+    }
+    return;
 }
 
 void merge_sort(int *a, int start, int end)
 {
-  if (start < end) {
-    int mid = (start + end)/2;
-    merge_sort(a, start, mid);
-    merge_sort(a, mid+1, end);
-    merge(a, start, mid, end);
-  }
+    if (start < end) {
+	int mid = (start + end)/2;
+	merge_sort(a, start, mid);
+	merge_sort(a, mid+1, end);
+	merge(a, start, mid, end);
+    }
 }
 
 
@@ -58,7 +59,7 @@ int main()
 {
     int a[MAX_ARRAY];
     int i = 0;
-    
+
     for(i = 0; i < MAX_ARRAY; i++) {
 	a[i] = rand()%100000;
 	printf("%d\t",a[i]);
